13a2bf48a21e7dbcc29aa75c131ee102265be8d1
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2023-07-18 06:46:40 +0530

    fixed dependacy issue

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -55,6 +55,7 @@ export default function LandingPage() {
   useEffect(() => {
     fetchEmojis();
     findUniqueCategories();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
